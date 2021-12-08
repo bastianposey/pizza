566c44943211bcd02d3af88ffe9dba8038b9376a
bastianposey <bastian_posey2@baylor.edu>
2021-12-08 17:00:58 -0600

    added toppings to site

--- MainPizza/urls.py
+++ MainPizza/urls.py
@@ -9,4 +9,5 @@ app_name = 'MainPizza'
 urlpatterns = [
     path('', views.index, name='index'),
     path('pizzas',views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza')
 ]
--- MainPizza/views.py
+++ MainPizza/views.py
@@ -13,3 +13,11 @@ def pizzas(request):
     context = {'pizzas': pizzas}
 
     return render(request, 'MainPizza/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    # foreign key can be accessed using '_set'
+    toppings = pizza.topping_set.all()
+    context = {'pizza': pizza, 'toppings':toppings}
+
+    return render(request, 'MainPizza/pizza.html', context)
